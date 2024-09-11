2ccfcd25ed93d73b6851ea66cab44363d6bdbf60
smartirshad <96040792+smartirshad@users.noreply.github.com>
2024-09-11 14:12:21 +0800

    initial commit

---
+++ CICDTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CICDTestProject
+//
+//  Created by Irshad Ahmed Qureshi on 11/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
